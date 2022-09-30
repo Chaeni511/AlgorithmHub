c27375aa5611b967accc71d6beeeccab6154ccf5
Chaeni511 <108424916+Chaeni511@users.noreply.github.com>
2022-09-30 16:45:15 +0900

    [Silver IV] Title: 카드2, Time: 268 ms, Memory: 49284 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,12 @@
+import sys
+from collections import deque
+N = int(sys.stdin.readline())
+Q = deque()
+for i in range(1, N+1):
+    Q.append(i)
+
+while len(Q) > 1:
+    Q.popleft()
+    Q.append(Q.popleft())
+
+print(Q[0])
