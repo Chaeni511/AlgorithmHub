018a1f93e045edb26152bb086a63128a8647c4ee
Chaeni511 <108424916+Chaeni511@users.noreply.github.com>
2022-10-12 02:26:03 +0900

    [Silver II] Title: 유기농 배추, Time: 104 ms, Memory: 32436 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,39 +1,30 @@
+import sys
 from collections import deque
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+T = int(sys.stdin.readline())
+for tc in range(T):
+    M, N, K = map(int, sys.stdin.readline().split())
+    arr = [[0] * M for _ in range(N)]
+    for _ in range(K):
+        m, n = map(int, sys.stdin.readline().split())
+        arr[n][m] = 1
 
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
-
-t = int(input())
-
-def bfs(graph, a, b):
-    queue = deque()
-    queue.append((a,b))
-    graph[a][b] = 0
-
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            if nx < 0 or nx >=n or ny < 0 or ny >= m:
-                continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = 0
-                queue.append((nx, ny))
-    return
-
-for i in range(t):
     cnt = 0
-    n, m, k = map(int,input().split())
-    graph = [[0]*m for _ in range(n)]
-
-    for j in range(k):
-        x, y = map(int, input().split())
-        graph[x][y] = 1
-
-    for a in range(n):
-        for b in range(m):
-            if graph[a][b] == 1:
-                bfs(graph, a, b)
+    stack = deque()
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] == 1:
+                arr[i][j] = 0
+                stack.append((i, j))
+                while stack:
+                    x, y = stack.popleft()
+                    for d in range(4):
+                        nx = x + dx[d]
+                        ny = y + dy[d]
+                        if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] == 1:
+                            arr[nx][ny] = 0
+                            stack.append((nx, ny))
                 cnt += 1
+
     print(cnt)
