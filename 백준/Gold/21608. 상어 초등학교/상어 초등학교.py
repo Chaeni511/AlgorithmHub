c5eaec75d931d20a82428848be0550b6b6cff18b
Chaeni511 <108424916+Chaeni511@users.noreply.github.com>
2022-11-09 10:31:06 +0900

    [Gold V] Title: 상어 초등학교, Time: 280 ms, Memory: 116856 KB -BaekjoonHub

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
@@ -12,16 +12,16 @@ for student in students:
         for j in range(N):
             blank = 0
             like = 0
-            if not classroom[i][j]:
-                for d in range(4):
+            if not classroom[i][j]:     # 해당 자리가 비었을 때
+                for d in range(4):      # 델타 탐색
                     ni, nj = i + di[d], j + dj[d]
                     if 0 <= ni < N and 0 <= nj < N:
-                        if classroom[ni][nj] in student[1:]:
+                        if classroom[ni][nj] in student[1:]:    # 좋아하는 사람
                             like += 1
-                        elif classroom[ni][nj] == 0:
+                        elif classroom[ni][nj] == 0:             # 빈칸
                             blank += 1
                 S.append((like, blank, i, j))
-    S.sort(reverse=True)
+    S.sort(reverse=True)    # 좋아하는 사람이 많은 순 / 빈칸이 많은 순으로 정렬
     s = S[0]
     classroom[s[2]][s[3]] = student[0]
 
