c147e05fd91c4fa03d5a1e7fb72f944f2dd69150
Chaeni511 <108424916+Chaeni511@users.noreply.github.com>
2023-01-26 23:36:16 +0900

    [Silver II] Title: 로또, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.py
@@ -0,0 +1,9 @@
+from itertools import combinations
+
+while True:
+    arr = list(map(int, input().split()))
+    if len(arr) == 1:
+        break
+    for i in combinations(arr[1:], 6):
+        print(*i)
+    print()
