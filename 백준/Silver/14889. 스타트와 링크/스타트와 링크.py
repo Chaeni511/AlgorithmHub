d599dc2a2199749624a5f585e658d3c35b69db70
Chaeni511 <108424916+Chaeni511@users.noreply.github.com>
2023-03-22 19:48:37 +0900

    [Silver II] Title: 스타트와 링크, Time: 7436 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,31 +1,24 @@
-import sys
-N = int(sys.stdin.readline())
-skill = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-player = list(range(N))
+from itertools import combinations
 
-
-result = 98754321
-
-for i in range(1<<N):
-    team1 = []
-    t1 = 0
-    team2 = []
-    t2 = 0
-    for j in range(N):
-        if i & (1<<j):
-            team1.append(j)
-    if len(team1) == N//2:
-        for p in player:
-            if p not in team1:
-                team2.append(p)
-        
-        for i in team1:
-            for j in team1:
-                t1 += skill[i][j]
-        
-        for i in team2:
-            for j in team2:
-                t2 += skill[i][j]
-        if abs(t1 - t2) < result:
-            result = abs(t1 - t2)
-print(result)
+N = int(input())
+S = [list(map(int, input().split())) for _ in range(N)]
+res = 987654321
+for comb in combinations([_ for _ in range(N)], N//2):
+    s1 = []
+    s2 = []
+    for i in range(N):
+        if i in comb:
+            s1.append(i)
+        else:
+            s2.append(i)
+    res1 = res2 = 0
+    for i in s1:
+        for j in s1:
+            if i != j:
+                res1 += S[i][j]
+    for i in s2:
+        for j in s2:
+            if i != j:
+                res2 += S[i][j]
+    res = min(res, abs(res1-res2))
+print(res)
