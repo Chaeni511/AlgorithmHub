86dc8b72be9e590a3400b095f9a4b96038b0ca8a
Chaeni511 <108424916+Chaeni511@users.noreply.github.com>
2023-04-11 21:55:00 +0900

    [Silver I] Title: 정수 삼각형, Time: 216 ms, Memory: 40632 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -7,7 +7,8 @@ dp[0][0] = triangle[0][0]
 for i in range(N):
     for j in range(len(dp[i])):
         if i+1 < N:
-            dp[i+1][j] = max(triangle[i+1][j] + dp[i][j], triangle[i+1][j], dp[i+1][j])
-            dp[i+1][j+1] = max(triangle[i + 1][j+1] + dp[i][j], triangle[i + 1][j+1], dp[i + 1][j+1])
-
+            # dp[i+1][j] = max(triangle[i+1][j] + dp[i][j], triangle[i+1][j], dp[i+1][j])
+            # dp[i+1][j+1] = max(triangle[i + 1][j+1] + dp[i][j], triangle[i + 1][j+1], dp[i + 1][j+1])
+            dp[i+1][j] = max(triangle[i+1][j] + dp[i][j], dp[i+1][j])
+            dp[i+1][j+1] = max(triangle[i + 1][j+1] + dp[i][j], dp[i + 1][j+1])
 print(max(dp[N-1]))
