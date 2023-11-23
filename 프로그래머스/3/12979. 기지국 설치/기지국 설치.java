059d660f5e0253416ccac227e88d73d8467608d0
Chaeni511 <108424916+Chaeni511@users.noreply.github.com>
2023-11-23 22:55:19 +0900

    [level 3] Title: 기지국 설치, Time: 0.92 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -10,10 +10,9 @@ class Solution {
         
         for (int i = 0; i < stations.length; i++) {
             
-            int s = stations[i] - w -1;
+            int s = stations[i] - w - 1;
                 
             if (start < s) {
-                // answer += Math.ceil((s-start)/coverage);
                 answer += (s-start)/coverage;
                 if ((s-start) % coverage > 0) {
                     answer++;
@@ -25,7 +24,6 @@ class Solution {
 
         answer += Math.ceil((n-start)/coverage);
         
-        // answer += (n-start)/coverage;
         if ((n-start) % coverage > 0) {
             answer++;
         }
